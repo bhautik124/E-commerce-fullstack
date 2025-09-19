3a21bf406dbf60da866cfa5d845c26ffe0d3356c
Bhautik Sangani <161152146+bhautik124@users.noreply.github.com>
2025-09-19 11:33:14 +0530

    Update Contect.jsx

--- frontend/src/components/Contect.jsx
+++ frontend/src/components/Contect.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:8000/contect/contectform",
+        "https://e-commerce-fullstack-backend-0y06.onrender.com/contect/contectform",
         formdata,
         { withCredentials: true }
       );
